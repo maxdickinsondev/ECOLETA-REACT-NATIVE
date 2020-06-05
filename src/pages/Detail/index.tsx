f0e39a452e8fe23aff8445d9c233bbb8eb0a6311
Max Dickinson <maxsantistadickinson@gmail.com>
2020-06-05 18:56:04 -0300

    Creating and styling item details page.

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import WhatsApp from 'react-native-vector-icons/FontAwesome';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import Constants from 'expo-constants';
+import { RectButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
 
 const Detail = () => {
-    return <View />
+    const navigation = useNavigation();
+
+    function handleNavigateBack() {
+      navigation.goBack();
+    }
+
+    return (
+      <>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={handleNavigateBack}>
+            <Icon name="arrow-left" size={20} color="#34cb79" />
+          </TouchableOpacity>
+
+          <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80' }} />
+          <Text style={styles.pointName}>Mercadão do seu João</Text>
+          <Text>Lâmpadas, Óleo de Cozinha</Text>
+
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}>Endereço</Text>
+            <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={() => {}}>
+            <WhatsApp name="whatsapp" size={20} color="#fff" />
+            <Text style={styles.buttonText}>WhatsApp</Text>
+          </RectButton>
+
+          <RectButton style={styles.button} onPress={() => {}}>
+            <Icon name="mail" size={20} color="#fff" />
+            <Text style={styles.buttonText}>E-mail</Text>
+          </RectButton>
+        </View>
+      </>
+    )
 };
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
-      paddingTop: 20,
+      paddingTop: 20 + Constants.statusBarHeight,
     },
   
     pointImage: {
