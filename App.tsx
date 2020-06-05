56650ef08aa876aa55bfdca8c4d3036509cac640
Max Dickinson <maxsantistadickinson@gmail.com>
2020-06-05 10:16:06 -0300

    Creating and styling home screen.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <> 
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
